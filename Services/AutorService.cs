fe5d924430a646391e46a95967a746fef5bb5a6c
ccezars <cesarelias1130@gmail.com>
2026-09-03 19:14:46 -0600

    Agregar archivos de proyecto.

---
+++ Services/AutorService.cs
@@ -0,0 +1,29 @@
+﻿using BibliotecaMVC.Models;
+
+namespace BibliotecaMVC.Services
+{
+    public class AutorService : IAutorService
+    {
+        private readonly List<Autor> _autores;
+
+        public AutorService()
+        {
+            _autores = new List<Autor>
+            {
+                new Autor { Id = 1, Nombre = "Gabriel García Márquez", Nacionalidad = "Colombiana", FechaNacimiento = new DateTime(1927, 3, 6) },
+                new Autor { Id = 2, Nombre = "Julio Cortázar", Nacionalidad = "Argentina", FechaNacimiento = new DateTime(1914, 8, 26) },
+                new Autor { Id = 3, Nombre = "Mario Vargas Llosa", Nacionalidad = "Peruana", FechaNacimiento = new DateTime(1936, 3, 28) }
+            };
+        }
+
+        public IEnumerable<Autor> ObtenerAutores()
+        {
+            return _autores;
+        }
+
+        public Autor? ObtenerAutorPorId(int id)
+        {
+            return _autores.FirstOrDefault(a => a.Id == id);
+        }
+    }
+}
---
+++ Services/AutorServiceAlternativo.cs
@@ -0,0 +1,29 @@
+﻿using BibliotecaMVC.Models;
+
+namespace BibliotecaMVC.Services
+{
+    public class AutorServiceAlternativo : IAutorService
+    {
+        private readonly List<Autor> _autores;
+
+        public AutorServiceAlternativo()
+        {
+            _autores = new List<Autor>
+            {
+                new Autor { Id = 10, Nombre = "Isabel Allende", Nacionalidad = "Chilena", FechaNacimiento = new DateTime(1942, 8, 2) },
+                new Autor { Id = 20, Nombre = "Jorge Luis Borges", Nacionalidad = "Argentina", FechaNacimiento = new DateTime(1899, 8, 24) },
+                new Autor { Id = 30, Nombre = "Pablo Neruda", Nacionalidad = "Chilena", FechaNacimiento = new DateTime(1904, 7, 12) }
+            };
+        }
+
+        public IEnumerable<Autor> ObtenerAutores()
+        {
+            return _autores.OrderBy(a => a.Nombre);
+        }
+
+        public Autor? ObtenerAutorPorId(int id)
+        {
+            return _autores.FirstOrDefault(a => a.Id == id);
+        }
+    }
+}
